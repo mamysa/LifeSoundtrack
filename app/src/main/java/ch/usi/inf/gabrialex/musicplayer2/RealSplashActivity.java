84ce447193d4dc7685c12b3c12789a8eb89f409b
mamysa <alexmamyshev@fastmail.com>
2017-12-13 00:29:29 +0100

    fix crash when permission not granted

--- app/src/main/java/ch/usi/inf/gabrialex/musicplayer2/RealSplashActivity.java
+++ app/src/main/java/ch/usi/inf/gabrialex/musicplayer2/RealSplashActivity.java
@@ -3,9 +3,9 @@ package ch.usi.inf.gabrialex.musicplayer2;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import ch.usi.inf.gabrialex.service.LibraryUpdateTask;
 
@@ -15,19 +15,16 @@ public class RealSplashActivity extends AppCompatActivity implements LibraryUpda
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_real_splash);
-        requestUserForPermissions();
-
-        LibraryUpdateTask libraryUpdateTask = new LibraryUpdateTask(this, this.getContentResolver());
-        libraryUpdateTask.setEventListener(this);
-        Thread thread = new Thread(libraryUpdateTask);
-        thread.start();
+        if (requestUserForPermissions()) {
+            this.startLibraryUpdateTask();
+        }
     }
 
 
     /**
      * requestUserForPermission
      */
-    private void requestUserForPermissions() {
+    private boolean requestUserForPermissions() {
         if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED || checkSelfPermission(android.Manifest.permission.ACCESS_COARSE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED || checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION)
@@ -36,7 +33,9 @@ public class RealSplashActivity extends AppCompatActivity implements LibraryUpda
 
             String[] req = {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET};
             requestPermissions(req, 10);
+            return false;
         }
+        return true;
     }
 
     /**
@@ -49,5 +48,18 @@ public class RealSplashActivity extends AppCompatActivity implements LibraryUpda
         Intent mainIntent = new Intent(RealSplashActivity .this,MainActivity.class);
         RealSplashActivity.this.startActivity(mainIntent);
         RealSplashActivity.this.finish();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        this.startLibraryUpdateTask();
+    }
+
+    private void startLibraryUpdateTask() {
+        LibraryUpdateTask libraryUpdateTask = new LibraryUpdateTask(this, this.getContentResolver());
+        libraryUpdateTask.setEventListener(this);
+        Thread thread = new Thread(libraryUpdateTask);
+        thread.start();
     }
 }
