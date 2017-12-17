e8e773697060542e322febc01b694159344a37d5
Gabry <g.abbate1@campus.unimib.it>
2017-12-17 15:01:54 +0100

    fixed problem with listItem not clickable while adding info button

--- app/src/main/java/ch/usi/inf/gabrialex/musicplayer2/SongAdapter.java
+++ app/src/main/java/ch/usi/inf/gabrialex/musicplayer2/SongAdapter.java
@@ -69,6 +69,7 @@ public class SongAdapter extends ArrayAdapter<Audio> {
             TextView ttd = (TextView) v.findViewById(R.id.toptextdata);
             TextView rank = (TextView) v.findViewById(R.id.ranktext);
             final ImageButton info = (ImageButton) v.findViewById(R.id.getRankInfoButton);
+            info.setFocusable(false);
             info.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
